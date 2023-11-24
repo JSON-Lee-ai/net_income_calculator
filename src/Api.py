5a25719e0a3b8b90397577f8bb145850a7eeb903
JSON-Lee-ai <jayson-lee.gillis@hotmail.be>
2023-11-24 20:02:54 +0100

    added bearer token auth

--- src/Api.py
+++ src/Api.py
@@ -1,4 +1,6 @@
-from modal import Stub, web_endpoint
+from modal import Secret, Stub, web_endpoint
+from fastapi import Depends, HTTPException, status, Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -7,10 +9,21 @@ import services.TaxCalculator as tc
 from App import App
 stub = Stub("net_income_calculator")
 
+auth_scheme = HTTPBearer()
+
 app = App(bf.BracketFactory(), tc.TaxCalculator)
-@stub.function()
+@stub.function(secret=Secret.from_name("web-auth-token"))
 @web_endpoint()
-def calculateNetIncome(gross_income: float, country: str) -> str:
+def calculateNetIncome(gross_income: float, country: str, token: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> str:
+    print(os.environ["WEB_AUTH"])
+
+    if token.credentials != os.environ["WEB_AUTH"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
     try:
         response = app.getNetIncome(gross_income, country)
     except:
